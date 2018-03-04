3533c9b694b8ed39d414f02a3b332ff9e5d0f9e8
Milan Ray <mray@e1z3r9p20.42.us.org>
2018-03-03 19:05:17 -0800

    Completed user.c and stuff

--- server.c
+++ server.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 int main(int argc, char **argv)
@@ -10,41 +10,41 @@ int main(int argc, char **argv)
 	int fd;
 	int sock;
 	int ret;
-
 	struct sockaddr_in addr;
-	int addr_len;
-
+	int				   addr_len;
 	addr_len = sizeof(addr);
-	char buf[1024];
+	char  buf[1024];
 	char *msg;
 	msg = "pong pong\n";
 	fd = 0;
 
-	if(argc != 2)
+	if (argc != 2)
 	{
-		printf("USAGE ./server [VALID PORT]");
+		printf("USAGE ./server [VALID PORT]\n");
 		exit(1);
 	}
-	if((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
 	{
-		printf("Creation of socket failed");
+		printf("Creation of socket failed\n");
 		exit(1);
 	}
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
 	addr.sin_port = htons(atoi(argv[1]));
-	if(bind(fd, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+	if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
 	{
-		printf("Binding of socket to hoper port failed");
+		printf("Binding of socket to hoper port failed\n");
 		exit(1);
 	}
-	if(listen(fd, 10) < 0)
+	if (listen(fd, 10) < 0)
 	{
-		printf("Unable to listen");
+		printf("Unable to listen\n");
 		exit(1);
 	}
-	if((sock = accept(fd, (struct sockaddr *) &addr, (socklen_t*) &addr_len)) < 0)
-		printf("Could not accept the connection");
+	printf("Listening on port %d\n...\n", atoi(argv[1]));
+	if ((sock = accept(fd, (struct sockaddr *)&addr, (socklen_t *)&addr_len)) <
+		0)
+		printf("Could not accept the connection\n");
 	ret = read(sock, buf, 1024);
 	printf("Sending: %s to server\n", buf);
 	send(sock, msg, strlen(msg), 0);
--- user.c
+++ user.c
@@ -4,42 +4,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <arpa/inet.h>
 #include <unistd.h>
 
 int main(int argc, char **argv)
 {
-	struct sockaddr_in address;
-	int sock = 0;
-	int valread;
 	struct sockaddr_in serv_addr;
-	char *hello = argv[3];
-	char buffer[1024] = {0};
+	int				   sock;
+	int				   ret;
+	char *			   msg;
+
+	msg = argv[3];
+	char buf[1024];
+
+	sock = 0;
+	if (argc != 3)
+	{
+		printf("USAGE ./server [HOST IP] [VALID PORT] [MSG]\n");
+		exit(1);
+	}
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		printf("\n Socket creation error \n");
-		return -1;
+		printf("Creation of socket failed\n");
+		return (0);
 	}
-
 	memset(&serv_addr, '0', sizeof(serv_addr));
-
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(atoi(argv[2]));
-
-	if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
+	if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
 	{
-		printf("\nInvalid address/ Address not supported \n");
-		return -1;
+		printf("Ip Address is not valid\n");
+		return (0);
 	}
-
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 	{
-		printf("\nConnection Failed \n");
-		return -1;
+		printf("Conection to host failed");
+		return (0);
 	}
-	send(sock , hello , strlen(hello) , 0 );
-	valread = read( sock , buffer, 1024);
-	printf("%s\n",buffer);
-	return 0;}
+	send(sock, msg, strlen(msg), 0);
+	ret = read(sock, buf, 1024);
+	printf("%s\n", buf);
+	return 0;
+}
